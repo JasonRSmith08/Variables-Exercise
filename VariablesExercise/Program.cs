63ee0f898a2a4fd4623470decb6814186399ca9d
Jason <JasonRSmith32@gmail.com>
2024-02-21 02:31:57 -0500

    Initial Commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string petName = " Charlie ";
+
+            int petAge = 11;
+
+            char firstInitial = 'C';
+
+            bool isBrown = false;
+
+            double petWeight = 70;
+
+            decimal petHeight = 37.6m;
+
+            Console.WriteLine($"Hello. My pets name is {petName}, his initial is {firstInitial}, and he is {petAge} years old.");
+            Console.WriteLine($"My pet weighs {petWeight} pounds, and he is {petHeight} inches tall.");
+            Console.WriteLine($"It is {isBrown.ToString().ToLower()} he is not the color brown.");
+
+
         }
     }
 }
